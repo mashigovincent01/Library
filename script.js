9e0593972e0510104d16b0f658a51e3394a4cb1f
mashigovincent01 <mashigovincent407@gmail.com>
2024-01-08 18:16:01 +0200

    done with the design

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+const myLibrary = [];
+
+function Book(){
+    //constructor
+    this.title = "",
+    this.author = "";
+    this.pages = 0;
+    this.isRead = false;
+}
+
+function addBookToLibrary(){
+
+    //do stuff here
+}
+
+
+
+
+
